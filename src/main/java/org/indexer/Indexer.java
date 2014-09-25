f16e6612cde15b36b82569b5b69a673015e9a7d1
mancmelou <mancmelou@me.com>
2014-09-24 21:55:31 -0700

    refacotor indexer to use factory

--- src/main/java/org/indexer/Indexer.java
+++ src/main/java/org/indexer/Indexer.java
@@ -1,29 +1,15 @@
 package org.indexer;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
-
-import java.io.File;
 import java.io.IOException;
 
 public class Indexer {
-    private InputReader input  = null;
-    private IndexWriter output = null;
+    private InputReader input;
+    private IndexWriter output;
 
-    public Indexer(String filePath) throws IOException {
-        Directory outputDir = FSDirectory.open(new File("./files/output"));
-
-        InputProvider provider   = new InputProvider(filePath);
-        Analyzer analyzer        = new StandardAnalyzer(Version.LUCENE_4_10_0);
-        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_0, analyzer).setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-
-        this.output = new IndexWriter(outputDir, config);
-        this.input  = new InputReader(provider);
+    public Indexer(String inputFilePath, String outputDirPath) throws IOException {
+        this.output = Factory.getOutputWriter(outputDirPath);
+        this.input  = Factory.getInputReader(inputFilePath);
     }
 
     public void run() throws IOException {
