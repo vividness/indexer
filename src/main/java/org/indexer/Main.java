7250afc633b5c8d80e7977069e0862264347dc90
mancmelou <mancmelou@me.com>
2014-09-26 23:53:00 -0700

    print usage if no args

--- src/main/java/org/indexer/Main.java
+++ src/main/java/org/indexer/Main.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length <= 0) {
+            Command.printUsage();
+            System.exit(1);
+        }
+
         if (args[0].toLowerCase().equals("create")) {
             Command.create(args[1], args[2]);
         } else if (args[0].toLowerCase().equals("drop")) {
