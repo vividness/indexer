6eedf6c8b82615525acc3649f65b3a84943da719
mancmelou <mancmelou@me.com>
2014-09-27 15:56:17 -0700

    add main commands to the command line utility

--- src/main/java/org/indexer/Main.java
+++ src/main/java/org/indexer/Main.java
@@ -4,17 +4,41 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args.length <= 0) {
+        if (args.length == 0) {
             Command.printUsage();
             System.exit(1);
         }
 
         if (args[0].toLowerCase().equals("create")) {
+            if (args[1].length() == 0 || args[2].length() == 0) {
+                Command.printUsage();
+                System.exit(1);
+            }
+
             Command.create(args[1], args[2]);
+        } else if (args[0].toLowerCase().equals("append")) {
+            if (args[1].length() == 0 || args[2].length() == 0) {
+                Command.printUsage();
+                System.exit(1);
+            }
+
+            Command.append(args[1], args[2]);
+        } else if (args[0].toLowerCase().equals("update")) {
+            if (args[1].length() == 0 || args[2].length() == 0) {
+                Command.printUsage();
+                System.exit(1);
+            }
+
+            Command.update(args[1], args[2]);
         } else if (args[0].toLowerCase().equals("drop")) {
+            if (args[1].length() == 0) {
+                Command.printUsage();
+                System.exit(1);
+            }
+
             Command.drop(args[1]);
-        } else {
-            Command.printUsage();
         }
+
+        System.exit(0);
     }
 }
