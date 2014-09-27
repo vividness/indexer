2e29c686454ece3d0322d6c69329d91da8701310
mancmelou <mancmelou@me.com>
2014-09-27 13:46:40 -0700

    add append command

--- src/main/java/org/indexer/Command.java
+++ src/main/java/org/indexer/Command.java
@@ -7,34 +7,46 @@ public class Command {
         Long startTime = System.currentTimeMillis();
         Indexer index  = new Indexer(inputFilePath, outputDirPath);
 
-        System.out.print("Indexing " + inputFilePath + " into " + outputDirPath + " ... ");
+        System.out.print("Creating index " + outputDirPath + " from " + inputFilePath + " ... ");
         index.create();
 
         System.out.format("Finished in %.2f seconds.\n", (float) (System.currentTimeMillis() - startTime) / 1000);
     }
 
-    public static void update(String inputFilePath, String outputDirPath) throws IOException {
-        new Indexer(inputFilePath, outputDirPath).create();
+    public static void append(String inputFilePath, String outputDirPath) throws IOException {
+        Long startTime = System.currentTimeMillis();
+        Indexer index  = new Indexer(inputFilePath, outputDirPath, Indexer.OpenMode.APPEND);
+
+        System.out.print("Updating index " + outputDirPath + " with " + inputFilePath + " ... ");
+        index.update();
+
+        System.out.format("Finished in %.2f seconds.\n", (float) (System.currentTimeMillis() - startTime) / 1000);
     }
 
     public static void drop(String outputDirPath) throws IOException {
+        Long startTime = System.currentTimeMillis();
+        System.out.print("Dropping index " + outputDirPath + " ... ");
+
         Indexer.drop(outputDirPath);
+        System.out.format("Finished in %.2f seconds.\n", (float) (System.currentTimeMillis() - startTime) / 1000);
     }
 
     public static void select(int limit) {
 
     }
 
-    public static void select(int offset, int limit) {
-
-    }
-
     public static void printUsage() {
-        System.out.println("Indexer, version 0.0");
+        System.out.println("Indexer, version 0.0\n");
+
+        System.out.println("Indexes all columns of the input CSV file.");
+        System.out.println("- All the fields in the file must be named in the following fashion [field_name]:[field_type] (e.g. id:integer,email:string).");
+        System.out.println("- The CSV file must have the first field named as id:integer.");
+
         System.out.println("\nUsage:\n");
-        System.out.println("- indexer create [input_file] [output_dir]");
-        System.out.println("- indexer update [input_file] [index_dir]");
-        System.out.println("- indexer drop [index_dir]");
-        System.out.println("- indexer select [all|first [n]|last [n]] from [index_dir] where [criteria]");
+
+        System.out.println("- indexer create [ input.csv ] [ index/dir ]");
+        System.out.println("- indexer update [ input.csv ] [ index/dir ]");
+        System.out.println("- indexer drop [ index/dir ]");
+        System.out.println("- indexer select [ all|first n|last n ] from [ index/dir ] where [ criteria ]");
     }
 }
