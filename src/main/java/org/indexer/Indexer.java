bb8abe5c3caf2421e993d1d21a08b4ad2383aa90
mancmelou <mancmelou@me.com>
2014-09-26 21:55:39 -0700

    add output writer wrapper

--- src/main/java/org/indexer/Indexer.java
+++ src/main/java/org/indexer/Indexer.java
@@ -1,20 +1,19 @@
 package org.indexer;
 
-import org.apache.lucene.index.IndexWriter;
 import java.io.IOException;
 
 public class Indexer {
     private InputReader input;
-    private IndexWriter output;
+    private OutputWriter output;
 
     public Indexer(String inputFilePath, String outputDirPath) throws IOException {
-        this.output = Factory.getOutputWriter(outputDirPath);
-        this.input  = Factory.getInputReader(inputFilePath);
+        this.input  = Components.getInputReader(inputFilePath);
+        this.output = Components.getOutputWriter(outputDirPath);
     }
 
     public void run() throws IOException {
         while (this.input.hasNext()) {
-            this.output.addDocument(this.input.next());
+            this.output.write(this.input.next());
         }
 
         this.output.close();
