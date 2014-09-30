479e68f2f677dc47ead73cafd95d354ce2f50290
mancmelou <mancmelou@me.com>
2014-09-29 23:03:39 -0700

    input reader layout - it sits between raw input and search class

--- src/main/java/org/indexer/search/InputReader.java
+++ src/main/java/org/indexer/search/InputReader.java
@@ -1,4 +1,11 @@
 package org.indexer.search;
 
+import java.io.IOException;
+
 class InputReader {
+    InputProvider provider;
+
+    public InputReader(String indexDirPath) throws IOException {
+        this.provider = Components.Lucene.getInputProvider(indexDirPath);
+    }
 }
