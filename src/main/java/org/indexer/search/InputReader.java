8056c34311b894af40d7e3a53a7f731be55b4f68
mancmelou <mancmelou@me.com>
2014-10-02 08:11:16 -0700

    basic functionality of searcher's input reader

--- src/main/java/org/indexer/search/InputReader.java
+++ src/main/java/org/indexer/search/InputReader.java
@@ -1,11 +1,31 @@
 package org.indexer.search;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-class InputReader {
-    InputProvider provider;
+class InputReader implements Iterator<String> {
+    private InputProvider provider;
+    private ArrayList<String> result;
 
     public InputReader(String indexDirPath) throws IOException {
         this.provider = Components.Lucene.getInputProvider(indexDirPath);
     }
+
+    public void query(String queryString) throws IOException {
+        this.result = this.provider.query(queryString);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public String next() {
+        return this.result.toString();
+    }
+
+    @Override
+    public void remove() {}
 }
