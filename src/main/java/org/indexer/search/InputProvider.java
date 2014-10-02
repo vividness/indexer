7f77a73850eaaeceff457631fe4e0dcaf62aef31
mancmelou <mancmelou@me.com>
2014-10-02 08:10:52 -0700

    basic input provider

--- src/main/java/org/indexer/search/InputProvider.java
+++ src/main/java/org/indexer/search/InputProvider.java
@@ -4,33 +4,49 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class InputProvider {
-    public static final int DEFAULT_NUM_RESULTS = 10;
-
+class InputProvider {
     private IndexSearcher searcher;
     private QueryParser parser;
 
     public InputProvider(String indexDirPath) throws IOException {
-        FSDirectory directory  = FSDirectory.open(new File(indexDirPath));
-        DirectoryReader reader = DirectoryReader.open(directory);
+        FSDirectory directory  = FSDirectory.open(new File(indexDirPath)); //todo: move to Components
+        DirectoryReader reader = DirectoryReader.open(directory); //todo: move to Components
 
         this.searcher = new IndexSearcher(reader); //todo: move to Components.Lucene
+        this.parser   = new QueryParser("id", new StandardAnalyzer()); //todo: Move to Components.Lucene
     }
 
-    public TopDocs query(String field, String queryString) throws IOException, ParseException {
-        return query(field, queryString, DEFAULT_NUM_RESULTS);
-    }
+    public ArrayList<String> query(String queryString) throws IOException {
+        Query query;
+        ArrayList<String> results = new ArrayList<String>();
 
-    public TopDocs query(String field, String queryString, int limit) throws IOException, ParseException {
-        this.parser = new QueryParser(field, new StandardAnalyzer());  //todo: Move to Components.Lucene
+        try {
+            query = this.parser.parse(queryString);
+        } catch (ParseException error) {
+            return null;
+        }
 
-        return this.searcher.search(this.parser.parse(queryString), limit);
+        TotalHitCountCollector hitsCollector = new TotalHitCountCollector();
+        this.searcher.search(query, hitsCollector);
+
+        int maxHits = Math.max(1, hitsCollector.getTotalHits());
+
+        TopScoreDocCollector docsCollector = TopScoreDocCollector.create(maxHits, true);
+        this.searcher.search(query, docsCollector);
+
+        ScoreDoc[] topDocs = docsCollector.topDocs().scoreDocs;
+
+        for (ScoreDoc hit : topDocs) {
+            results.add(this.searcher.doc(hit.doc).get("id"));
+        }
+
+        return results;
     }
 }
