491f6de792c600d4b0f9d3557d0e58b7248659c2
mancmelou <mancmelou@me.com>
2014-10-02 08:12:08 -0700

    use printUsageAndExit() so the code looks a bit cleaner

--- src/main/java/org/indexer/Main.java
+++ src/main/java/org/indexer/Main.java
@@ -5,35 +5,30 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            Command.printUsage();
-            System.exit(1);
+            Command.printUsageAndExit(1);
         }
 
         if (args[0].toLowerCase().equals("create")) {
             if (args[1].length() == 0 || args[2].length() == 0) {
-                Command.printUsage();
-                System.exit(1);
+                Command.printUsageAndExit(1);
             }
 
             Command.create(args[1], args[2]);
         } else if (args[0].toLowerCase().equals("append")) {
             if (args[1].length() == 0 || args[2].length() == 0) {
-                Command.printUsage();
-                System.exit(1);
+                Command.printUsageAndExit(1);
             }
 
             Command.append(args[1], args[2]);
         } else if (args[0].toLowerCase().equals("update")) {
             if (args[1].length() == 0 || args[2].length() == 0) {
-                Command.printUsage();
-                System.exit(1);
+                Command.printUsageAndExit(1);
             }
 
             Command.update(args[1], args[2]);
         } else if (args[0].toLowerCase().equals("drop")) {
             if (args[1].length() == 0) {
-                Command.printUsage();
-                System.exit(1);
+                Command.printUsageAndExit(1);
             }
 
             Command.drop(args[1]);
