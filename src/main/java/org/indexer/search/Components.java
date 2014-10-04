9bb74167703fadacdd3fbfd8a0f56da764ce0189
mancmelou <mancmelou@me.com>
2014-10-04 14:34:12 -0700

    isolating components

--- src/main/java/org/indexer/search/Components.java
+++ src/main/java/org/indexer/search/Components.java
@@ -1,15 +1,27 @@
 package org.indexer.search;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
 import java.io.IOException;
 
 abstract class Components {
-    public static class Lucene {
-        public static InputProvider getInputProvider(String indexDirPath) throws IOException {
-            return new InputProvider(indexDirPath);
-        }
-    }
-
     public static InputReader getInputReader(String indexDirPath) throws IOException {
         return new InputReader(indexDirPath);
+    }
+
+    public static QueryParser getQueryParser() {
+        return new QueryParser(null, new StandardAnalyzer());
+    }
+
+    public static IndexSearcher getIndexSearcher(String indexDirPath) throws IOException {
+        FSDirectory directory  = FSDirectory.open(new File(indexDirPath));
+        DirectoryReader reader = DirectoryReader.open(directory);
+
+        return new IndexSearcher(reader);
     }
 }
