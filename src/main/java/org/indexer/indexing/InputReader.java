7338f4ee61b4e472269f53f47c46486cfc24894f
mancmelou <mancmelou@me.com>
2014-10-06 20:58:48 -0700

    simplifying indexing logic

--- src/main/java/org/indexer/indexing/InputReader.java
+++ src/main/java/org/indexer/indexing/InputReader.java
@@ -1,59 +1,42 @@
 package org.indexer.indexing;
 
+import org.apache.commons.csv.CSVParser;
 import org.apache.lucene.document.*;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 
 final class InputReader implements Iterator<Document> {
-    private InputProvider provider;
-    private Document      document;
-    private Field         fields[];
+    private CSVParser input;
+    private Document  document;
+    private Field     fields[];
+
+    private Iterator<CSVRecord> inputIterator;
 
     public InputReader(String inputFilePath) throws IOException {
-        this.provider = Components.CSV.getInputProvider(inputFilePath);
+        this.input    = Components.CSV.getCsvParser(inputFilePath);
         this.document = Components.Lucene.getEmptyDocument();
-        this.fields   = Components.Lucene.getEmptyDocumentFields(provider.getFields().size());
 
+        this.inputIterator = input.iterator();
+
+        // todo: this can be one method
+        this.fields   = Components.Lucene.getEmptyDocumentFields(this.input.getHeaderMap().size());
         this.initDocumentFields();
-    }
-
-    private void initDocumentFields() {
-        Object[] inputFieldNames = this.provider.getFields().keySet().toArray();
-        Object[] inputFieldTypes = this.provider.getFields().values().toArray();
-
-        for (int i = 0; i < this.fields.length; i++) {
-            String fieldName = (String) inputFieldNames[i];
-            String fieldType = (String) inputFieldTypes[i];
-
-            if (fieldType.equals("integer")) {
-                if (fieldName.toLowerCase().equals("id")) {
-                    fields[i] = new IntField(fieldName, 0, Field.Store.YES);
-                } else {
-                    fields[i] = new IntField(fieldName, 0, Field.Store.YES);
-                }
-            } else if (fieldType.equals("float")) {
-                fields[i] = new FloatField(fieldName, 0F, Field.Store.YES);
-            } else {
-                fields[i] = new StringField(fieldName, "", Field.Store.YES);
-            }
-
-            this.document.add(fields[i]);
-        }
     }
 
     @Override
     public Document next() {
-        CSVRecord row = this.provider.next();
+        CSVRecord row = this.inputIterator.next();
 
-        for (int i = 0; i < fields.length; i++) {
-            if (fields[i].getClass() == IntField.class) {
-                fields[i].setIntValue(Integer.valueOf(row.get(i)));
-            } else if (fields[i].getClass() == FloatField.class) {
-                fields[i].setFloatValue(Float.valueOf(row.get(i)));
+        for (int i = 0; i < this.fields.length; i++) {
+            if (this.fields[i].getClass() == IntField.class) {
+                this.fields[i].setIntValue(Integer.valueOf(row.get(i)));
+            } else if (this.fields[i].getClass() == FloatField.class) {
+                this.fields[i].setFloatValue(Float.valueOf(row.get(i)));
             } else {
-                fields[i].setStringValue(row.get(i));
+                this.fields[i].setStringValue(row.get(i));
             }
         }
 
@@ -62,9 +45,25 @@ final class InputReader implements Iterator<Document> {
 
     @Override
     public boolean hasNext() {
-        return this.provider.hasNext();
+        return this.inputIterator.hasNext();
     }
 
     @Override
     public void remove() {}
+
+    private void initDocumentFields() {
+        String[] inputFieldNames = this.getFieldNames();
+
+        for (int i = 0; i < inputFieldNames.length; i++) {
+            this.fields[i] = new StringField(inputFieldNames[i], "", Field.Store.YES);
+
+            this.document.add(this.fields[i]);
+        }
+    }
+
+    private String[] getFieldNames() {
+        Object[] fieldNames = this.input.getHeaderMap().keySet().toArray();
+
+        return Arrays.copyOf(fieldNames, fieldNames.length, String[].class);
+    }
 }
