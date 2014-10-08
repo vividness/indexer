8bf685b6ec4257b3b25a25e8523e4c1b4cb835b2
mancmelou <mancmelou@me.com>
2014-10-07 19:09:06 -0700

    add support for custom unique id

--- src/main/java/org/indexer/indexing/OutputWriter.java
+++ src/main/java/org/indexer/indexing/OutputWriter.java
@@ -7,23 +7,35 @@ import org.apache.lucene.index.Term;
 import java.io.IOException;
 
 final class OutputWriter {
-    private IndexWriter output;
+    private IndexWriter  output;
+    private final String ID_FIELD;
 
     public OutputWriter(String outputDirPath) throws IOException {
-        this.output = Components.Lucene.getIndexWriter(outputDirPath);
+        this.output   = Components.Lucene.getIndexWriter(outputDirPath);
+        this.ID_FIELD = "id";
     }
 
     public OutputWriter(String outputDirPath, Indexer.OpenMode mode) throws IOException {
-        this.output = Components.Lucene.getIndexWriter(outputDirPath, mode);
+        this.output   = Components.Lucene.getIndexWriter(outputDirPath, mode);
+        this.ID_FIELD = "id";
+    }
+
+    public OutputWriter(String outputDirPath, Indexer.OpenMode mode, String idField) throws IOException {
+        this.output   = Components.Lucene.getIndexWriter(outputDirPath, mode);
+        this.ID_FIELD = idField;
+    }
+
+    public OutputWriter(String outputDirPath, String idField) throws IOException {
+        this.output   = Components.Lucene.getIndexWriter(outputDirPath);
+        this.ID_FIELD = idField;
     }
 
     public void insert(Document doc) throws IOException {
         this.output.addDocument(doc);
     }
 
-    // todo: make possible to specify different unique identifier
     public void update(Document doc) throws IOException {
-        this.output.updateDocument(new Term("id", doc.get("id")), doc);
+        this.output.updateDocument(new Term(this.ID_FIELD, doc.get(this.ID_FIELD)), doc);
     }
 
     public void delete() throws IOException {
