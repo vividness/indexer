f2a31b4bbe43fbb991ec2acdd72b2476bf408ce8
mancmelou <mancmelou@me.com>
2014-10-07 18:50:14 -0700

    drop the "provider" term

--- src/main/java/org/indexer/indexing/OutputWriter.java
+++ src/main/java/org/indexer/indexing/OutputWriter.java
@@ -7,30 +7,31 @@ import org.apache.lucene.index.Term;
 import java.io.IOException;
 
 final class OutputWriter {
-    private IndexWriter provider;
+    private IndexWriter output;
 
     public OutputWriter(String outputDirPath) throws IOException {
-        this.provider = Components.Lucene.getIndexWriter(outputDirPath);
+        this.output = Components.Lucene.getIndexWriter(outputDirPath);
     }
 
     public OutputWriter(String outputDirPath, Indexer.OpenMode mode) throws IOException {
-        this.provider = Components.Lucene.getIndexWriter(outputDirPath, mode);
+        this.output = Components.Lucene.getIndexWriter(outputDirPath, mode);
     }
 
     public void insert(Document doc) throws IOException {
-        this.provider.addDocument(doc);
+        this.output.addDocument(doc);
     }
 
+    // todo: make possible to specify different unique identifier
     public void update(Document doc) throws IOException {
-        this.provider.updateDocument(new Term("id", doc.get("id")), doc);
+        this.output.updateDocument(new Term("id", doc.get("id")), doc);
     }
 
     public void delete() throws IOException {
-        this.provider.deleteAll();
-        this.provider.commit();
+        this.output.deleteAll();
+        this.output.commit();
     }
 
     public void close() throws IOException {
-        this.provider.close();
+        this.output.close();
     }
 }
