56cd346643732c2d23649ea9659dae122906031b
mancmelou <mancmelou@me.com>
2014-10-14 07:57:42 -0700

    add javadoc

--- src/main/java/org/indexer/search/Components.java
+++ src/main/java/org/indexer/search/Components.java
@@ -10,14 +10,33 @@ import java.io.File;
 import java.io.IOException;
 
 abstract class Components {
+    /**
+     * Opens an existing index and returns the instance of InputReader.
+     *
+     * @param indexDirPath Path to the dir where your index is stored.
+     * @return Input reader instance which implements methods for querying the index.
+     * @throws IOException
+     */
     public static InputReader getInputReader(String indexDirPath) throws IOException {
         return new InputReader(indexDirPath);
     }
 
+    /**
+     * Returns Lucene's QueryParser instance.
+     *
+     * @return Query parser instance used in parsing lucene queries.
+     */
     public static QueryParser getQueryParser() {
         return new QueryParser(null, new StandardAnalyzer());
     }
 
+    /**
+     * Returns and instance of Lucene's index searcher.
+     *
+     * @param indexDirPath Path to the dir where your index is stored.
+     * @return Index searcher instance.
+     * @throws IOException
+     */
     public static IndexSearcher getIndexSearcher(String indexDirPath) throws IOException {
         FSDirectory directory  = FSDirectory.open(new File(indexDirPath));
         DirectoryReader reader = DirectoryReader.open(directory);
