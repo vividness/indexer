dc6b89144e8ab37919e9311f97359d7a95d72b18
mancmelou <mancmelou@me.com>
2014-10-14 07:58:17 -0700

    add javadoc

--- src/main/java/org/indexer/indexing/Indexer.java
+++ src/main/java/org/indexer/indexing/Indexer.java
@@ -3,21 +3,51 @@ package org.indexer.indexing;
 import java.io.IOException;
 
 public class Indexer {
+    /**
+     * Open modes that the Indexer class supports
+     */
     public enum OpenMode { CREATE, APPEND, CREATE_OR_APPEND }
 
-    private InputReader  input;
-    private OutputWriter output;
+    /**
+     * This instance reads the input file. Use Components to create a new instance.
+     */
+    private final InputReader input;
 
+    /**
+     * This instance creates the index. Use Components to create a new instance.
+     */
+    private final OutputWriter output;
+
+    /**
+     * Initializes the input, creates an output instance in CREATE open mode.
+     *
+     * @param inputFilePath Path to the file that is going to be indexed.
+     * @param outputDirPath Path to the directory where the index will be stored.
+     * @throws IOException
+     */
     public Indexer(String inputFilePath, String outputDirPath) throws IOException {
         this.input  = Components.getInputReader(inputFilePath);
         this.output = Components.getOutputWriter(outputDirPath);
     }
 
+    /**
+     * Initializes the input, creates an output instance in specified open mode.
+     *
+     * @param inputFilePath Path to the file that is going to be indexed.
+     * @param outputDirPath Path to the directory where the index will be stored.
+     * @param mode CREATE, APPEND or CREATE_OR_APPEND.
+     * @throws IOException
+     */
     public Indexer(String inputFilePath, String outputDirPath, OpenMode mode) throws IOException {
         this.input  = Components.getInputReader(inputFilePath);
         this.output = Components.getOutputWriter(outputDirPath, mode);
     }
 
+    /**
+     * Takes a next entry from the input file and adds the entry to the index.
+     *
+     * @throws IOException
+     */
     public void insert() throws IOException {
         while (this.input.hasNext()) {
             this.output.insert(this.input.next());
@@ -26,6 +56,12 @@ public class Indexer {
         this.output.close();
     }
 
+    /**
+     * Takes a next entry from the input file and tries to update an existing document in the index.
+     * If an existing document cannot be found for update, a new document will be added to the index.
+     *
+     * @throws IOException
+     */
     public void update() throws IOException {
         while (this.input.hasNext()) {
             this.output.update(this.input.next());
@@ -34,10 +70,16 @@ public class Indexer {
         this.output.close();
     }
 
+    /**
+     * Removes the contents of an index.
+     *
+     * @param outputDirPath Path to the directory where the index will be stored.
+     * @throws IOException
+     */
     public static void drop(String outputDirPath) throws IOException {
         OutputWriter output = Components.getOutputWriter(outputDirPath);
 
-        output.delete();
+        output.drop();
         output.close();
     }
 }
