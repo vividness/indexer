f3134633019401fc5c23738d5fb06b09f45f877d
mancmelou <mancmelou@me.com>
2014-10-14 07:58:06 -0700

    add java doc

--- src/main/java/org/indexer/indexing/OutputWriter.java
+++ src/main/java/org/indexer/indexing/OutputWriter.java
@@ -7,42 +7,99 @@ import org.apache.lucene.index.Term;
 import java.io.IOException;
 
 final class OutputWriter {
-    private IndexWriter  output;
+    /**
+     * Index writer instance. Use Components to create a new instance.
+     */
+    private final IndexWriter  output;
+
+    /**
+     * Document unique identifier field.
+     */
     private final String ID_FIELD;
 
+    /**
+     * Opens index for writing with default CREATE mode and "id" as the unique identifier.
+     *
+     * @param outputDirPath Path to the dir where the index will be created.
+     * @throws IOException
+     */
     public OutputWriter(String outputDirPath) throws IOException {
         this.output   = Components.Lucene.getIndexWriter(outputDirPath);
         this.ID_FIELD = "id";
     }
 
+    /**
+     * Opens index for writing with "id" as the unique identifier.
+     *
+     * @param outputDirPath Path to the dir where the index will be created.
+     * @param mode Index open mode.
+     * @throws IOException
+     */
     public OutputWriter(String outputDirPath, Indexer.OpenMode mode) throws IOException {
         this.output   = Components.Lucene.getIndexWriter(outputDirPath, mode);
         this.ID_FIELD = "id";
     }
 
+    /**
+     * Opens index for writing.
+     *
+     * @param outputDirPath Path to the dir where the index will be created.
+     * @param mode Index open mode.
+     * @param idField Unique identifier for each document that will be added to the index.
+     * @throws IOException
+     */
     public OutputWriter(String outputDirPath, Indexer.OpenMode mode, String idField) throws IOException {
         this.output   = Components.Lucene.getIndexWriter(outputDirPath, mode);
         this.ID_FIELD = idField;
     }
 
+    /**
+     * Opens index for writing in CREATE open mode.
+     *
+     * @param outputDirPath Path to the dir where the index will be created.
+     * @param idField Unique identifier for each document that will be added to the index.
+     * @throws IOException
+     */
     public OutputWriter(String outputDirPath, String idField) throws IOException {
         this.output   = Components.Lucene.getIndexWriter(outputDirPath);
         this.ID_FIELD = idField;
     }
 
+    /**
+     * Inserts a new document into the index.
+     *
+     * @param doc Document object that to be added to the index.
+     * @throws IOException
+     */
     public void insert(Document doc) throws IOException {
         this.output.addDocument(doc);
     }
 
+    /**
+     * Removes and adds an updated version for the document identified by it's id field
+     *
+     * @param doc New version of the document to index.
+     * @throws IOException
+     */
     public void update(Document doc) throws IOException {
         this.output.updateDocument(new Term(this.ID_FIELD, doc.get(this.ID_FIELD)), doc);
     }
 
-    public void delete() throws IOException {
+    /**
+     * Removes all entries from an index.
+     *
+     * @throws IOException
+     */
+    public void drop() throws IOException {
         this.output.deleteAll();
         this.output.commit();
     }
 
+    /**
+     * Closes the index writer.
+     *
+     * @throws IOException
+     */
     public void close() throws IOException {
         this.output.close();
     }
