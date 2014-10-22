baf8c68d8e4348a0a5deb06cdb4690e4427df196
mancmelou <mancmelou@me.com>
2014-10-21 19:58:06 -0700

    add indexer tests

---
+++ src/test/java/org/indexer/indexing/IndexerTest.java
@@ -0,0 +1,29 @@
+package org.indexer.indexing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndexerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDrop() throws Exception {
+
+    }
+}
